19101c28b9afcb6537411de25ce5259c111a29ef
simon-arapoglu <simon.arapoglu@anevis-solutions.com>
2023-09-11 15:02:02 +0200

    aaa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,24 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        String a = "";
-        String b = "";
-        String x = "";
-
-        String password = "blue";
-
-        for (; ; ) {  // Noncompliant; end condition omitted
-            // ...
-        }
-    }
-
-    int foo(int a) {
-        int b = 12;
-        if (a == 1) {
-            return b;
-        }
-        return b;  // Noncompliant
+        String password = "1234";
     }
 
 }
