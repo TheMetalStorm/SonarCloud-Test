64e41bd35bc53945ee66a3fe90853dfd399e51f0
Simon Arapoglu <simon.arapoglu@anevis-solutions.com>
2023-10-25 15:34:08 +0200

    added test

---
+++ src/test/java/Testy.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Testy {
+
+    @Test
+    public void testy() {
+        Assertions.assertEquals(1, 1);
+    }
+}
